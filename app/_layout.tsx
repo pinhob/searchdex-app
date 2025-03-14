b3807886bc9b3790fe52e13782191d34764f97ae
pinhob <20286747+pinhob@users.noreply.github.com>
2025-03-14 00:56:46 -0300

    feat: add axios and react query and fetch data from api

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,13 @@
-import { Stack } from "expo-router";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import React from 'react';
+import { Stack } from 'expo-router';
+
+const queryClient = new QueryClient();
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Stack />
+    </QueryClientProvider>
+  );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -3,12 +3,21 @@ import { View } from "react-native";
 import SearchTitle from "./components/SearchTitle";
 import SearchInput from "./components/SearchInput";
 import SearchButton from "./components/SearchButton";
+import api from "@/lib/api";
 
 export default function Index() {
   const [searchQuery, setSearchQuery] = useState("");
 
   const handleSearch = () => {
     console.log("Searching for:", searchQuery);
+
+    api.get(`/abilities/${searchQuery}`)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   };
 
   return (
