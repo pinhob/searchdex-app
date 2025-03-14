9b5d07b27d8594204571cafd610bf7875594dcd7
pinhob <20286747+pinhob@users.noreply.github.com>
2025-03-14 15:10:04 -0300

    style: add shake animation to Loading component

--- app/components/Loading.tsx
+++ app/components/Loading.tsx
@@ -1,13 +1,53 @@
-import React from "react";
-import { Image, Text } from "react-native";
+import React, { useEffect } from "react";
+import { Image, Text, Animated, Easing } from "react-native";
 
 export default function Loading() {
+  const rotateAnim = new Animated.Value(0);
+  
+  const startShakeAnimation = () => {
+    Animated.loop(
+      Animated.sequence([
+        Animated.timing(rotateAnim, {
+          toValue: -1,
+          duration: 200,
+          easing: Easing.linear,
+          useNativeDriver: true,
+        }),
+        Animated.timing(rotateAnim, {
+          toValue: 1,
+          duration: 400,
+          easing: Easing.linear,
+          useNativeDriver: true,
+        }),
+        Animated.timing(rotateAnim, {
+          toValue: 0,
+          duration: 200,
+          easing: Easing.linear,
+          useNativeDriver: true,
+        }),
+      ])
+    ).start();
+  };
+  
+  useEffect(() => {
+    startShakeAnimation();
+  }, []);
+  
+  const rotation = rotateAnim.interpolate({
+    inputRange: [-1, 0, 1],
+    outputRange: ['-10deg', '0deg', '10deg']
+  });
   
   return (
     <>
-      <Image 
+      <Animated.Image 
         source={require("../../assets/images/pokeball.svg")} 
-        style={{ width: 70, height: 70, marginTop: 20 }}
+        style={{ 
+          width: 70, 
+          height: 70, 
+          marginTop: 20,
+          transform: [{ rotate: rotation }]
+        }}
         alt="Loading abilities..."
       />
     </>
