d205bcb28b7ea693ad0f9ccc987a9b4e4479d5e0
pinhob <20286747+pinhob@users.noreply.github.com>
2025-03-14 15:46:43 -0300

    feat: add ReactQueryDevTools

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,13 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useReactQueryDevTools } from '@dev-plugins/react-query';
 import React from 'react';
 import { Stack } from 'expo-router';
 
 const queryClient = new QueryClient();
 
 export default function RootLayout() {
+  useReactQueryDevTools(queryClient);
+
   return (
     <QueryClientProvider client={queryClient}>
       <Stack screenOptions={{ headerShown: false }} />
