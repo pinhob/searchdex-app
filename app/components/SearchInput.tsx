e898ccba524252a6ff860dc1eb3472ce486a5e72
pinhob <20286747+pinhob@users.noreply.github.com>
2025-03-14 14:54:22 -0300

    style: SearchInput

--- app/components/SearchInput.tsx
+++ app/components/SearchInput.tsx
@@ -1,26 +1,52 @@
 import React from "react";
-import { TextInput } from "react-native";
+import { View, TextInput as RNTextInput } from "react-native";
+import styled from "styled-components/native";
+import { Ionicons } from "@expo/vector-icons";
 
 interface SearchInputProps {
   value: string;
   onChangeText: (text: string) => void;
 }
 
-export default function SearchInput ({ value, onChangeText }: SearchInputProps) {
+export default function SearchInput({ value, onChangeText }: SearchInputProps) {
   return (
-    <TextInput
-      style={{
-        height: 40,
-        borderColor: 'gray',
-        borderWidth: 1,
-        marginTop: 20,
-        marginBottom: 20,
-        paddingLeft: 10,
-        width: '80%',
-      }}
-      placeholder="Type your search query"
-      value={value}
-      onChangeText={onChangeText}
-    />
+    <InputContainer>
+      <SearchIcon name="search" size={20} color="#757575" />
+      <TextInput
+        placeholder="Pesquise um Pokémon..."
+        placeholderTextColor={props => props.theme.colors.lightText}
+        value={value}
+        onChangeText={onChangeText}
+      />
+    </InputContainer>
   );
-};
+}
+
+const InputContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
+  width: 85%;
+  max-width: 500px;
+  height: 44px;
+  background-color: ${props => props.theme.colors.background};
+  margin: 20px 0;
+  padding: 0 16px;
+  border-radius: ${props => props.theme.borderRadius.lg}px;
+  border-width: 1px;
+  border-color: #dfe1e5;
+  shadow-opacity: 0.2;
+  shadow-radius: 3px;
+  shadow-color: ${props => props.theme.colors.shadow};
+  shadow-offset: 0px 1px;
+  elevation: 2;
+`;
+
+const SearchIcon = styled(Ionicons)`
+  margin-right: 8px;
+`;
+
+const TextInput = styled(RNTextInput)`
+  flex: 1;
+  font-size: ${props => props.theme.fontSizes.md}px;
+  color: ${props => props.theme.colors.text};
+`;
