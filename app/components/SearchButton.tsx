2e7f32b58ea68abbdace8756fd1b2fc4b76a069b
pinhob <20286747+pinhob@users.noreply.github.com>
2025-03-14 15:01:38 -0300

    style: SearchButton

--- app/components/SearchButton.tsx
+++ app/components/SearchButton.tsx
@@ -1,10 +1,33 @@
 import React from "react";
-import { Button } from "react-native";
+import { TouchableOpacity, Text } from "react-native";
+import styled from "../styles/styled";
 
 interface SearchButtonProps {
   onPress: () => void;
 }
 
-export default function SearchButton ({ onPress }: SearchButtonProps) {
-  return <Button title="Search" onPress={onPress} />;
+export default function SearchButton({ onPress }: SearchButtonProps) {
+  return (
+    <StyledButton onPress={onPress}>
+      <ButtonText>Buscar habilidades</ButtonText>
+    </StyledButton>
+  );
 };
+
+const StyledButton = styled.TouchableOpacity`
+  background-color: #F2F7F2;
+  padding: 12px 24px;
+  border-radius: ${props => props.theme.borderRadius.lg}px;
+  margin-top: 10px;
+  shadow-opacity: 0.2;
+  shadow-radius: 2px;
+  shadow-color: ${props => props.theme.colors.shadow};
+  shadow-offset: 0px 1px;
+  elevation: 2;
+`;
+
+const ButtonText = styled.Text`
+  color: ${props => props.theme.colors.text};
+  font-size: ${props => props.theme.fontSizes.md}px;
+  font-weight: 500;
+`;
